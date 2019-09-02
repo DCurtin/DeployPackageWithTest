ea0688f9a25b2a79b6cfe720114932f682d5225b
Dalton Curtin <daltoncurtin@gmail.com>
2019-09-02 17:19:20 -0400

    changing logic to check for True text

--- DeployPackageWithTest.psm1
+++ DeployPackageWithTest.psm1
@@ -10,7 +10,7 @@
     {
         Write-Host "Available paramters: `n* userName [required] `n* package [required] `n* checkOnly [optional]`n"
         Write-Host "e.g. DeployPackageWithTest -userName dcurtin@midlandira.com.dcurtin -package .\manifest\package.xml"
-        Write-Host "e.g. DeployPackageWithTest -userName dcurtin@midlandira.com.dcurtin -package .\manifest\package.xml -checkOnly $true"
+        Write-Host "e.g. DeployPackageWithTest -userName dcurtin@midlandira.com.dcurtin -package .\manifest\package.xml -checkOnly True"
         return
     }
 
@@ -23,7 +23,7 @@
     [xml]$packake_import = Get-Content $package
     $tests = $($packake_import.Package.types.members -match "Test") -join ','
     
-    if($checkOnly)
+    if($checkOnly -eq "True")
     {
         sfdx force:source:deploy -x $package -l RunSpecifiedTests -r $tests -u $userName -c
     }else{
