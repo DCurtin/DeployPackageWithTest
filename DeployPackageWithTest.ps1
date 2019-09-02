b617f1c7f3bfc06cb8eee469e37e30d4a47703dd
Dalton Curtin <daltoncurtin@gmail.com>
2019-09-02 16:53:04 -0400

    Added script

---
+++ DeployPackageWithTest.ps1
@@ -0,0 +1,19 @@
+﻿function DeployPackageWithTest
+{
+    param(
+        $userName,
+        $package,
+        $checkOnly=$false
+    )
+
+    #$tests = $(Get-ChildItem -Recurse "*Test.cls").name.replace('.cls', '') -join ','
+    [xml]$packake_import = Get-Content $package
+    $tests = $($packake_import.Package.types.members -match "Test") -join ','
+    
+    if($checkOnly)
+    {
+        sfdx force:source:deploy -x $package -l RunSpecifiedTests -r $tests -u $userName -c
+    }else{
+        sfdx force:source:deploy -x $package -l RunSpecifiedTests -r $tests -u $userName
+    }
+}
